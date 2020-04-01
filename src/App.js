248a4cec525fca28923478281fa6484a1adab4b8
Queen <queensform@Queens-MacBook-Air.local>
2020-04-01 10:56:04 -0700

    init project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Form from './Form'
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Stimulus Package Calculator</h1>
+      <p>What will this calculator help you do? The COVID-19 or Coronavirus pandemic has affected all of us. Below is a calulator to help you determine whether your will receive any benefits for the most recently passed stimulus package bill. </p>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Form.js
@@ -0,0 +1,121 @@
+import React from 'react'
+
+export default class Form extends React.Component{
+    constructor(props){
+        super(props); 
+        this.state={
+            value: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleChange(event) {
+        this.setState({value: event.target.value});
+      }
+    
+      handleSubmit(event) {
+        
+        event.preventDefault();
+      }
+    
+    render(){
+        // creating Event Listner on Click of Button //
+var event1 = document.getElementById("button");
+event1.addEventListener('click',calculate,false);
+
+// creating Event Listner on Click of Button //
+var event2 = document.getElementById("button1");
+event2.addEventListener('click',reset,false);
+
+
+// Creating Function to Calculate Salary //
+
+function calculate()
+{
+    var salary = document.getElementById("amount").value;
+    
+// If statement for salary under 10600
+    if ( salary <= 10600)
+        {
+            document.getElementById("GrossSalary").innerHTML ="<li>"+ "Your Gross Salary is " +salary+"."+"</li>";
+            
+            document.getElementById("TaxFreeAllowance").innerHTML ="<li>"+ "Your Tax Free Allowance "+ 10600. +"</li>";
+                                                                                                                                                     
+            document.getElementById("NetSalary").innerHTML ="<li>"+ "Your Net Salary is "+salary + "." + "</li>" ;
+
+        }
+    
+// statement for salary between 10600 & 31000
+    
+   else if (( salary >10600) && salary <= 31000 )
+        {
+            
+            var Taxableamount =( salary - 10600 ); 
+            
+            var Tax = Taxableamount * 0.20;
+            
+            var NetSalary = salary-Tax;
+            
+            document.getElementById("GrossSalary").innerHTML ="<li>"+"Your Gross Salary is" +salary+"."+"</li>";
+            
+            document.getElementById("TaxFreeAllowance").innerHTML ="<li>"+"Your Tax Free Allowance "+ 10600+"."+"</li>";
+
+            document.getElementById("TaxAt20").innerHTML ="<li>" + "Your Tax At 20% is " + Tax+"."+"</li>";
+
+            document.getElementById("NetSalary").innerHTML ="<li>"+ "Your Net Salary is "+NetSalary+"."+"</li>";
+
+        }
+    
+    // for salary abouve 31000
+    else 
+        {
+            
+            var Taxableamount =( 31000 - 10600 ); 
+            
+            var Tax = Taxableamount * 0.20;
+            
+            var Taxableamount2 = ( salary - 31000 )
+            
+            var Tax1 = Taxableamount2 *.40;
+            
+            var totaltax = Tax + Tax1;
+            
+            var NetSalary = salary-totaltax;
+            
+            
+            document.getElementById("GrossSalary").innerHTML ="<li>"+"Your Gross Salary is " +salary+"."+"</li>";
+            
+            document.getElementById("TaxFreeAllowance").innerHTML ="<li>"+"Your Tax Free Allowance "+ 10600+"."+"</li>";
+
+            document.getElementById("TaxAt20").innerHTML = "<li>"+"Your Tax At 20% is " + Tax+"."+"</li>";
+
+            document.getElementById("TaxAt40").innerHTML ="<li>"+ "Your Tax at 40% is " + Tax1+"."+"</li>";
+
+            document.getElementById("NetSalary").innerHTML ="<li>"+ "Your Net Salary is "+NetSalary+"."+"</li>";
+
+        }
+    
+    
+}
+
+function reset()
+
+{
+    window.location.reload();
+}
+
+        return(
+            <>
+                <form  onSubmit={this.handleSubmit} style ={{display: "flex", flexDirection: "row"}}>
+                    <label htmlFor="single"><input name="single"  type="radio"/> Single </label>
+                    <label htmlFor="married"><input name="married" type="radio"/> Married </label>
+                    <label htmlFor="head"><input name="head" type="radio"/> Head of Household </label>
+                    <label htmlFor="income"><input name="income" type="number"/> Adjusted Gross Income </label>
+                    <label htmlFor="children"><input name="children" type="number"/>  Number of Children Claimed as Dependents </label>  
+                    <input type="submit" value="Submit" />
+                </form>
+                <p> Your estimated stimulus check = $</p>
+            </>
+        )
+    }
+}
